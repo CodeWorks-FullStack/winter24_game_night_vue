78eea2ef5da474f9cba7e1414412b6f4c73737a0
Jeremy Fowler <jembyfowler@gmail.com>
2024-12-02 09:46:54 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import { AppState } from './AppState.js';
+
+
+</script>
+
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+  </main>
+  <footer class="bg-dark text-light">
+    Made with 💖 by CodeWorks
+  </footer>
+</template>
+
+<style lang="scss">
+// NOTE do not remove this or bootstrap will break!
+@import '@/assets/scss/main.scss';
+</style>
